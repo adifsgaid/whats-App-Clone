d7e0d927a5bca9db15a2a53292bc8475eebd5965
adifsgaid <infosgaid@gmail.com>
2020-12-15 11:48:40 +0100

    need to fix the refresh login

--- src/Chat.js
+++ src/Chat.js
@@ -7,20 +7,32 @@ import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import MicIcon from '@material-ui/icons/Mic';
 import { useParams } from 'react-router-dom';
 import db from './firebase';
+import { useStateValue } from './StateProvider';
+import firebase from 'firebase';
+
 
 function Chat() {
     const [input, setInput] = useState('');
     const [seed, setSeed] = useState('');
     const { roomId } = useParams();
     const [roomName, setRoomName ] = useState('')
-    
+    const [messages, setMessages] = useState([]);
+    const [{ user }, dispatch] = useStateValue();
+
     useEffect(() => {
         if (roomId){
             db.collection('rooms').doc(roomId).onSnapshot(snapshot => (
                 setRoomName(snapshot.data().name)
+            ))  
+            db.collection('rooms')
+            .doc(roomId)
+            .collection('messages')
+            .orderBy('timestamp', 'asc')
+            .onSnapshot(snapshot => (
+                setMessages(snapshot.docs.map(doc => doc.data()))
             ))
-        }
-    }, [roomId])
+    }
+}, [roomId])
 
     useEffect(() =>{
         setSeed(Math.floor(Math.random() * 500))
@@ -29,9 +41,16 @@ function Chat() {
     const sendMessage = (e) => {
         e.preventDefault();
         console.log('you typed >>>>', input)
-        setInput('')
+        
+        db.collection('rooms').doc(roomId).collection('messages').add({
+            message: input,
+            name: user.displayName,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        })
+
+        setInput('');
     }
-    
+
     return (
         <div className='chat'>
             <div className='chat__header'>
@@ -40,7 +59,9 @@ function Chat() {
              </IconButton> 
                 <div className='chat__headerInfo'>
                     <h3>{roomName}</h3>
-                    <p>LAST seen</p>
+                    <p>Last seen {''} {new Date(
+                            messages[messages.length - 1]?.timestamp?.toDate()
+                        ).toUTCString()}</p>
                 </div>
                 
                 <div className='chat__headerRight'>
@@ -59,13 +80,18 @@ function Chat() {
             </div>
             
             <div className='chat__body'>
+                {messages.map(message =>(
+   
                 <p className={`chat__message 
-                ${true && 'chat__receiver'}`}> 
-                <span className='chat__name'>adif</span>
-                hello
-                <span className='chat__timestamp'>16:45pm</span> 
+                ${message.name === user.displayName && 'chat__receiver'}`}> 
+                <span className='chat__name'>{message.name}</span>
+                {message.message}
+                <span className='chat__timestamp'>
+                    {new Date(message.timestamp?.toDate()).toUTCString()}
+                    </span> 
                 </p>
-                
+                ))}
+            
             </div>
             
             <div className='chat__footer'>
